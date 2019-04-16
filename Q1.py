8696dbf71d13b71efdf0691c6f71ea03d4212bd7
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-16 07:58:06 -0400

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -11,7 +11,7 @@ from keras.layers import Conv2D, MaxPooling2D
 import pandas
 
 batch_size = 128
-num_classes = 10
+num_classes = 15
 epochs = 50
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -33,7 +33,7 @@ x_test /= 255
 keras.preprocessing.image.ImageDataGenerator(featurewise_center=False, samplewise_center=True, featurewise_std_normalization=True, samplewise_std_normalization=True, zca_whitening=True, zca_epsilon=1e-06, rotation_range=0, width_shift_range=0.0, height_shift_range=0.0, brightness_range=None, shear_range=0.0, zoom_range=0.0, channel_shift_range=0.0, fill_mode='nearest', cval=0.1, horizontal_flip=True, vertical_flip=False, rescale=None, preprocessing_function=None, data_format=None, validation_split=0.5, dtype=None)
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
-model.add(Activation('tanh'))
+model.add(Activation('relu'))
 
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
@@ -42,6 +42,9 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 
 model.add(Conv2D(64, (3, 3)))
