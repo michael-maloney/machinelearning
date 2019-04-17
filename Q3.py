41a138b15d2951528a06559329345f3533fc886d
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-17 13:32:46 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -4,6 +4,7 @@ Q3 Inception Module for CIFAR-10 dataset
 
 '''
 import keras
+from keras import regularizers
 from keras.models import Model
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense
