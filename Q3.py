dbc1176db05f42daaf8d3b2c286782c2be30dffe
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-17 14:21:29 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -50,16 +50,16 @@ output = keras.layers.concatenate([volume_1, volume_2, volume_3,
 
 
 # Create Volumes for the Inception module
-volume_a = Conv2D(64, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l1(0.06))(output)
+volume_a = Conv2D(64, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l1(0.06),activity_regularizer = regularizer.l2(0.05))(output)
 
 volume_b = Conv2D(96, (1,1), padding='same', activation='relu')(output)
-volume_b = Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.06))(volume_b)
+volume_b = Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.06),activity_regularizer = regularizer.l2(0.05))(volume_b)
 
 volume_c = Conv2D(16, (1,1), padding='same', activation='relu')(output)
-volume_c = Conv2D(32, (5,5), padding='same', activation='relu', kernel_regularizer=regularizers.l1(0.06))(volume_c)
+volume_c = Conv2D(32, (5,5), padding='same', activation='relu', kernel_regularizer=regularizers.l1(0.06),activity_regularizer = regularizer.l2(0.05))(volume_c)
 
 volume_d = MaxPooling2D((3,3), strides=(1,1), padding='same')(output)
-volume_d = Conv2D(32, (2,2), padding='same', activation='relu', kernel_regularizer=regularizers.l1(0.06))(volume_d)
+volume_d = Conv2D(32, (2,2), padding='same', activation='relu', kernel_regularizer=regularizers.l1(0.06),activity_regularizer = regularizer.l2(0.05))(volume_d)
 
 
 # Concatenate all volumes of the Inception module
