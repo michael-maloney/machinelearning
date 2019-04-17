18b59bbef66ae82f9b9058963b9b7d0a746d0ddf
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-17 13:40:21 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -31,16 +31,16 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu')(input_img)
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(input_img)
 
 volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
-volume_2 = Conv2D(128, (3,3), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.05))(volume_2)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_2)
 
 volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(32, (5,5), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.05))(volume_3)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_3)
 
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
-volume_4 = Conv2D(32, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.05))(volume_4)
+volume_4 = Conv2D(32, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_4)
 
 
 # Concatenate all volumes of the Inception module
@@ -54,7 +54,7 @@ model = Model(inputs = input_img, outputs = out)
 print(model.summary())
 
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=512)
 
 
