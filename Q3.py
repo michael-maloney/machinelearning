f3b5c226d6dac8254fb34cd72917f847018e2691
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-17 13:47:38 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -31,21 +31,24 @@ input_img = Input(shape = (32, 32, 3))
 
 
 # Create Volumes for the Inception module
-volume_1 = Conv2D(64, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(input_img)
+volume_1 = Conv2D(64, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.05))(input_img)
 
 volume_2 = Conv2D(96, (1,1), padding='same', activation='relu')(input_img)
-volume_2 = Conv2D(128, (3,3), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_2)
+volume_2 = Conv2D(128, (3,3), padding='same', activation='relu', kernel_regularizer=regularizers.l2(0.05))(volume_2)
 
 volume_3 = Conv2D(16, (1,1), padding='same', activation='relu')(input_img)
-volume_3 = Conv2D(32, (5,5), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_3)
+volume_3 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_3)
 
 volume_4 = MaxPooling2D((3,3), strides=(1,1), padding='same')(input_img)
-volume_4 = Conv2D(32, (1,1), padding='same', activation='relu',kernel_regularizer=regularizers.l2(0.09))(volume_4)
+volume_4 = Conv2D(32, (1,1), padding='same', activation='relu')(volume_4)
+
+volume_5 = Conv2D(16, (1,1), padding='same', activation='relu')(volume_4)
+volume_5 = Conv2D(32, (5,5), padding='same', activation='relu')(volume_5)
 
 
 # Concatenate all volumes of the Inception module
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
-                                             volume_4], axis = 3)
+                                             volume_5], axis = 3)
 output = Flatten()(inception_module)
 out    = Dense(10, activation='softmax')(output)
 
