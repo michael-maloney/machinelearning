022eab673879c69c9633de703f36cac28d055e54
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-21 14:20:11 -0400

    Update Q5_Train.py

--- Q5_Train.py
+++ Q5_Train.py
@@ -58,12 +58,12 @@ y = to_categorical(y, num_classes=vocab_size)
 ##############################################################################
 
 model = Sequential()
-model.add(LSTM(500  ,input_shape=(X.shape[1], X.shape[2])))
-model.add(Dropout(0.45))
+model.add(LSTM(512  ,input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.35))
 model.add(Dense(vocab_size, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X, y, epochs=20 , verbose=1, batch_size=1000 )
+model.fit(X, y, epochs=20 , verbose=1, batch_size=5000 )
 
 # Save and test using code from the Q4_Test
 model.save('LargeLSTM_model.h5')
