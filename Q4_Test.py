f803dc4a24ce782c9006c7132d83da531ef1e843
michael-maloney <36242647+michael-maloney@users.noreply.github.com>
2019-04-21 20:54:58 -0400

    Update Q4_Test.py

--- Q4_Test.py
+++ Q4_Test.py
@@ -15,8 +15,8 @@ n_chars_to_predict = 500
 seq_length = 100
 
 # load the model and mapping
-model = load_model('LargeLSTM_model_512_4096_50.h5')
-mapping = load(open('LargeLSTM1_mapping.pkl', 'rb'))
+model = load_model('LargeLSTM_model.h5')
+mapping = load(open('LargeLSTM_mapping.pkl', 'rb'))
 
 
 # Make predictions
@@ -26,11 +26,19 @@ for k in range(n_chars_to_predict):
     # truncate sequences to a fixed length
     encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
     # one hot encode
-    encoded = encoded/float(len(mapping))
-    encoded = np.reshape(encoded, (encoded.shape[0], seq_length, 1))
-
-    
     # encoded = to_categorical(encoded, num_classes=len(mapping))
+    
+    # Tried without converting len(mapping) to float, would only output "...mmmmmmmm..."
+    encoded /= float(len(mapping))
+    
+    # numpy.reshape(a, newshape, order='C'
+    # Reshapes the array so that it is easily processed because it is different from 
+    # the Q4 model -- The model was coded differently so it needs to be parsed differently
+    encoded = numpy.reshape(encoded,    #array to bereshaped
+                            (encoded.shape[0],  #newshape
+                             seq_length, 
+                             1))
+    
     # predict character
     yhat = model.predict_classes(encoded, verbose=0)
     
